0e1a52aaa94bce25738b86cab53654bb4c31a71c
FraterSKS <jackson.a.furr@gmail.com>
2022-10-17 22:28:30 -0400

    question card layout

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,13 +1,14 @@
 const Question = (props) => {
   return (
-    <div className="card question">
+    <div className="card question m-3">
       <div className="card-body">
         <div className="card-text">
           {props.param1} X {props.param2}
         </div><hr />
         <div className="card-text">
           <label htmlFor="answer">Answer</label><br />
-          <input type="number" name="answer" />
+          <input type="number" name="answer" /><br />
+          <button className="mt-2">Submit</button>
         </div>
       </div>
     </div>
